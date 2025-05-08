b0e73df129b950e85813601891c5aae5cdc15bda
Dam <Dam@Info1-Pc19>
2025-05-08 11:28:27 +0200

    Programación Orientada Objetos

---
+++ Ejemplo1/POO/ProgramaciónObjetos/Ejemplo_Objetos.java
@@ -0,0 +1,62 @@
+package ProgramaciónObjetos;
+
+public class Ejemplo_Objetos {
+
+	int edad;
+	double altura;
+	double peso;
+	boolean pelo;
+	String voz;
+	
+	public Ejemplo_Objetos(int edad, double altura, double peso, boolean pelo, String voz) {
+		super();
+		this.edad = edad;
+		this.altura = altura;
+		this.peso = peso;
+		this.pelo = pelo;
+		this.voz = voz;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+		
+	public double getAltura() {
+		return altura;
+	}
+		
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public double getPeso() {
+		return peso;
+	}
+		
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+		
+	public boolean isPelo() {
+		return pelo;
+	}
+		
+	public void setPelo(boolean pelo) {
+		this.pelo = pelo;
+	}
+		
+	public String getVoz() {
+		return voz;
+	}
+	
+	public void setVoz(String voz) {
+		this.voz = voz;
+	}
+	
+
+
+}
